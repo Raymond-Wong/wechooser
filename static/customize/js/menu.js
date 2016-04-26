58924e85566e8ac9b9d97b20de6f2944c2cfa7fe
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 22:09:18 +0800

    auto submit

--- static/customize/js/menu.js
+++ static/customize/js/menu.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#menuForm').submit(function() {
-    var menu = $('#menu').text();
-    alert(menu);
-    var params = {"menu" : menu};
-    $.post('/wechat/menu', params, function(res) {
-  	  console.log(res);
-    });
+    var menu = $('#menu').html();
+    if (confirm(menu))
+      var params = {"menu" : menu};
+      $.post('/wechat/menu', params, function(res) {
+  	    console.log(res);
+      });
   });
 });
