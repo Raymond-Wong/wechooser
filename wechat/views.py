760e54a881eb9b8d7ec67e5fa035abe41fea17a8
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 21:24:54 +0800

    auto submit

--- wechat/views.py
+++ wechat/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 reload(sys)
-sys.setdefaultencoding('gbk')
+sys.setdefaultencoding('utf-8')
 
 import hashlib
 import time
@@ -67,7 +67,9 @@ def editMenu(request, token):
   host = 'api.weixin.qq.com'
   path = '/cgi-bin/menu/create?access_token=' + token.token
   method = 'POST'
-  params = json.loads(request.POST.get('menu'))
+  # params = json.loads(request.POST.get('menu'))
+  params = {'button' : []}
+  params['button'].append({"name" : "今日歌曲", "type" : "click", "key" : "asdf"})
   logger('DEBUG', u'自定义菜单创建: ' + request.POST.get('menu', u'post数据中没有menu'))
   res = send_request(host, path, method, port=80, params=params)
   if res[0]:
