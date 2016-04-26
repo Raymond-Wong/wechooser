8601ffe700f888314dcfbd41db0d0c46773badc1
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 16:08:30 +0800

    auto submit

--- wechat/utils.py
+++ wechat/utils.py
@@ -92,8 +92,9 @@ def sendMsgTo(token, _to, msgType, content):
   host = 'api.weixin.qq.com'
   path = '/cgi-bin/message/custom/send?access_token=' + token.token
   method = 'POST'
-  return send_request(host=host, path=path, method=method, port=80, params=params)
-
+  res = send_request(host=host, path=path, method=method, port=80, params=params)
+  logger('DEBUG', u'发送一条客服消息：' + str(res))
+  return res
 
 def xml2dict(root):
   dictionary = {}
