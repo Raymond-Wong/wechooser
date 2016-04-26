e6f564a2746fa33f5b6f00246d49e1a58bfc89a5
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 15:58:15 +0800

    auto submit

--- wechat/utils.py
+++ wechat/utils.py
@@ -90,7 +90,7 @@ def sendMsgTo(token, _to, msgType, content):
     }
   }
   host = 'api.weixin.qq.com'
-  path = '/cgi-bin/message/custom/send?access_token=' + token
+  path = '/cgi-bin/message/custom/send?access_token=' + token.token
   method = 'POST'
   return send_request(host, path, method, params)
 
