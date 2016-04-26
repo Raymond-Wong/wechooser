03f477f50341a16185bbb639c096a1786f3c635e
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 21:58:25 +0800

    auto submit

--- static/customize/js/menu.js
+++ static/customize/js/menu.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-  var menu = {};
-  menu['button'] = [];
-  menu['button'].push({"type" : "click", "name" : "今日歌曲", "key" : "V1001_TODAY_MUSIC"});
-  menu['button'].push({"name" : "menu", "sub_button" : []});
-  menu['button'][1]['sub_button'].push({'url' : 'http://www.baidu.com', 'type' : 'view', 'name' : 'search'});
-  var params = {"menu" : JSON.stringify(menu)};
-  console.log(params);
-  $.post('/wechat/menu', params, function(res) {
-  	console.log(res);
+  $('#menuForm').submit(function() {
+    var menu = $('#menu').text();
+    var params = {"menu" : menu};
+    console.log(params);
+    $.post('/wechat/menu', params, function(res) {
+  	  console.log(res);
+    });
   });
 });
