d65ce602994b95fdd0f4ac82e7c4b2604f71abb5
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 21:12:01 +0800

    auto submit

--- static/customize/js/menu.js
+++ static/customize/js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var menu = {};
   menu['button'] = [];
-  menu['button'].push({"type" : "click", "name" : "song", "key" : "V1001_TODAY_MUSIC"});
+  menu['button'].push({"type" : "click", "name" : "今日歌曲", "key" : "V1001_TODAY_MUSIC"});
   menu['button'].push({"name" : "menu", "sub_button" : []});
   menu['button'][1]['sub_button'].push({'url' : 'http://www.baidu.com', 'type' : 'view', 'name' : 'search'});
   var params = {"menu" : JSON.stringify(menu)};
