822f157be258c426170a5203c44c566602aa3766
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 21:09:23 +0800

    auto submit

--- static/customize/js/menu.js
+++ static/customize/js/menu.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   var menu = {};
   menu['button'] = [];
-  menu['button'].push({"type" : "click", "name" : "今日歌曲", "key" : "V1001_TODAY_MUSIC"});
-  menu['button'].push({"name" : "菜单", "sub_button" : []});
-  menu['button'][1]['sub_button'].push({'url' : 'http://www.baidu.com', 'type' : 'view', 'name' : '搜索'});
+  menu['button'].push({"type" : "click", "name" : "song", "key" : "V1001_TODAY_MUSIC"});
+  menu['button'].push({"name" : "menu", "sub_button" : []});
+  menu['button'][1]['sub_button'].push({'url' : 'http://www.baidu.com', 'type' : 'view', 'name' : 'search'});
   var params = {"menu" : JSON.stringify(menu)};
   console.log(params);
   $.post('/wechat/menu', params, function(res) {
