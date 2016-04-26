2b3fe48bbce99134dfa367b7b5bf3b951257aa55
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-26 22:02:13 +0800

    auto submit

--- wechat/views.py
+++ wechat/views.py
@@ -67,6 +67,7 @@ def editMenu(request, token):
   host = 'api.weixin.qq.com'
   path = '/cgi-bin/menu/create?access_token=' + token.token
   method = 'POST'
+  logger('DEBUG', '获取到menu: ' + request.POST.get('menu'))
   params = json.loads(request.POST.get('menu'))
   res = send_request(host, path, method, port=80, params=params)
   if res[0]:
