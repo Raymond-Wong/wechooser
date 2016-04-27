9aeadf671d5d29b6c198be717ab226d6a9d7f9ab
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-27 14:56:48 +0800

    auto submit

--- static/customize/js/material.js
+++ static/customize/js/material.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.post('/wechat/getMaterial', {'type' : 'image'}, function(res) {
+  $.post('/wechat/getMaterial', {'type' : 'image', 'offset' : 0, 'count' : 1}, function(res) {
   	$('#material').text(res);
   });
 });
