1d0acb9183b747924383c4e98a14f866421ca2fe
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-04-28 17:37:24 +0800

    auto submit

--- wechooser/utils.py
+++ wechooser/utils.py
@@ -42,7 +42,8 @@ def send_request(host, path, method, port=443, params={}):
   res = client.getresponse()
   if not res.status == 200:
     return False, res.status
-  resDict = json.loads(res.read().decode())
+  resDict = json.loads(res.read())
+  logger('DEBUG', res.read())
   if 'errcode' in resDict.keys() and resDict['errcode'] == 40001:
     raise PastDueException('access token past due')
   if 'errcode' in resDict.keys() and resDict['errcode'] != 0:
