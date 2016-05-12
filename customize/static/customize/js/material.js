3b7d7afaf78094b8a199296ac646e857cac14359
RaymondWong <549425036@qq.com>
2016-05-12 16:58:00 +0800

    auto submit

--- customize/static/customize/js/material.js
+++ customize/static/customize/js/material.js
@@ -48,7 +48,10 @@ var listenInput = function() {
   var materialText = $('#materialText')[0];
   $('#materialText').keydown(function(evt) {
     if (evt.keyCode == '13') {
-      insertIntoCaret('materialText', '\n');
+      if (materialText.find('div').length <= 0) {
+        materialText.html('<div>' + materialText.html() + '</div>');
+      }
+      // insertIntoCaret('materialText', '\n');
     }
   });
   if (materialText.addEventListener) {
@@ -73,9 +76,9 @@ var updateRemainChar = function() {
 var textHandler = function() {
   var tmpDiv = parseFirstLine($('#materialText')).clone();
   // 在每个div前面加一个换行符
-  // tmpDiv.find('div').each(function() {
-  //   $(this).prepend('\n');
-  // })
+  tmpDiv.find('div').each(function() {
+    $(this).prepend('\n');
+  })
   tmpDiv.find('img').each(function() {
     var face = $(this).attr('name');
     $(this).before(face);
