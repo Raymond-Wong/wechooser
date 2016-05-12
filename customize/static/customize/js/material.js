8dfd69aa21f46179b342d973ebed6944e2a81397
RaymondWong <549425036@qq.com>
2016-05-12 16:52:13 +0800

    auto submit

--- customize/static/customize/js/material.js
+++ customize/static/customize/js/material.js
@@ -48,7 +48,7 @@ var listenInput = function() {
   var materialText = $('#materialText')[0];
   $('#materialText').keydown(function(evt) {
     if (evt.keyCode == '13') {
-      // insertIntoCaret('materialText', '<nl />');
+      insertIntoCaret('materialText', '\n');
     }
   });
   if (materialText.addEventListener) {
--- static/customize/js/materialBox.js
+++ static/customize/js/materialBox.js
@@ -359,7 +359,7 @@ var remainCharAmount = function() {
   var materialText = $('#materialTextInputArea');
   materialText.keydown(function(evt) {
     if (evt.keyCode == '13') {
-      insertIntoCaret('materialTextInputArea', '\n');
+      insertIntoCaret('materialTextInputArea', '<nl/>');
     }
   });
   if (materialText[0].addEventListener) {
