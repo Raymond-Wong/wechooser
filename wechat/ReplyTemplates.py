2d4beb1e83f1ed86fa5cc4a812b39c9264cde97d
RaymondWong <549425036@qq.com>
2016-05-12 15:20:37 +0800

    auto submit

--- wechat/ReplyTemplates.py
+++ wechat/ReplyTemplates.py
@@ -42,7 +42,8 @@ class Template:
 class TextTemplate(Template):
   def __init__(self, ToUserName='', FromUserName='', CreateTime=time.time(), MsgType='text', Content=''):
     Template.__init__(self, ToUserName=ToUserName, FromUserName=FromUserName, MsgType=MsgType, CreateTime=CreateTime)
-    self.Content = Content.strip('\t').strip('\r').strip('\n')
+    wechooser.utils.logger('DEBUG', 'text template\'s content: %s' % content)
+    self.Content = Content.strip().strip('\t').strip('\r').strip('\n')
   def toReply(self):
     dic = self.toDic()
     dic['Content'] = self.Content
