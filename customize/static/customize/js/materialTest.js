3d2bd70d1b69397c33ba02245d39af037ba7cc6b
RaymondWong <549425036@qq.com>
2016-05-13 15:53:44 +0800

    删除无用文件

--- customize/static/customize/js/materialTest.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $.post('/wechat/getMaterial', {'type' : 'voice', 'count' : 10, 'offset' : 0}, function(res) {
-  	console.log(res);
-  	$('#material').html(JSON.stringify(res));
-  });
-});
