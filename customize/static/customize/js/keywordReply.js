2ad54c5662ccfad0ac2269915835f2d06bd1e983
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-01 19:30:00 +0800

    auto submit

--- customize/static/customize/js/keywordReply.js
+++ customize/static/customize/js/keywordReply.js
@@ -128,37 +128,38 @@ var bindSaveRuleAction = function() {
 }
 
 var closeRuleAction = function(saveRuleBtn) {
-  	var rule = saveRuleBtn.parents('.ruleDetailWrapper');
-  	var ruleShort = $(rule.siblings('.ruleShortWrapper')[0]);
-  	var name = $(rule.find('input[name="ruleName"]')[0]).val();
-  	var textAmount = $(rule.find('.textAmount')[0]).text();
-  	var imageAmount = $(rule.find('.imageAmount')[0]).text();
-  	var voiceAmount = $(rule.find('.voiceAmount')[0]).text();
-  	var videoAmount = $(rule.find('.videoAmount')[0]).text();
-  	var newsAmount = $(rule.find('.newsAmount')[0]).text();
-  	var totalAmount = parseInt(textAmount) + parseInt(imageAmount);
-  	totalAmount += (parseInt(voiceAmount) + parseInt(videoAmount));
-  	totalAmount += (parseInt(newsAmount));
-  	var keywords = [];
-  	$(rule.find('.keyword')).each(function() {
-  	  var keyword = $(this).children('.val').html();
-  	  keywords.push(keyword);
-  	});
-    $(rule.find('.ruleName')[0]).text(name);
-  	$(ruleShort.find('.ruleName')[0]).text(name);
-  	$(ruleShort.find('.totalAmount')[0]).text(totalAmount);
-  	$(ruleShort.find('.textAmount')[0]).text(textAmount);
-  	$(ruleShort.find('.imageAmount')[0]).text(imageAmount);
-  	$(ruleShort.find('.voiceAmount')[0]).text(voiceAmount);
-  	$(ruleShort.find('.videoAmount')[0]).text(videoAmount);
-  	$(ruleShort.find('.newsAmount')[0]).text(newsAmount);
-  	var keywordsWrapper = $(ruleShort.find('.keywordsWrapper .val')[0]);
-  	keywordsWrapper.html('');
-  	for (var i = 0; i < keywords.length; i++) {
-  	  keywordsWrapper.append('<font>' + keywords[i] + '</font>');
-  	}
-  	rule.hide();
-  	ruleShort.show();
+  window.location.href = window.location.href;
+  	// var rule = saveRuleBtn.parents('.ruleDetailWrapper');
+  	// var ruleShort = $(rule.siblings('.ruleShortWrapper')[0]);
+  	// var name = $(rule.find('input[name="ruleName"]')[0]).val();
+  	// var textAmount = $(rule.find('.textAmount')[0]).text();
+  	// var imageAmount = $(rule.find('.imageAmount')[0]).text();
+  	// var voiceAmount = $(rule.find('.voiceAmount')[0]).text();
+  	// var videoAmount = $(rule.find('.videoAmount')[0]).text();
+  	// var newsAmount = $(rule.find('.newsAmount')[0]).text();
+  	// var totalAmount = parseInt(textAmount) + parseInt(imageAmount);
+  	// totalAmount += (parseInt(voiceAmount) + parseInt(videoAmount));
+  	// totalAmount += (parseInt(newsAmount));
+  	// var keywords = [];
+  	// $(rule.find('.keyword')).each(function() {
+  	//   var keyword = $(this).children('.val').html();
+  	//   keywords.push(keyword);
+  	// });
+   //  $(rule.find('.ruleName')[0]).text(name);
+  	// $(ruleShort.find('.ruleName')[0]).text(name);
+  	// $(ruleShort.find('.totalAmount')[0]).text(totalAmount);
+  	// $(ruleShort.find('.textAmount')[0]).text(textAmount);
+  	// $(ruleShort.find('.imageAmount')[0]).text(imageAmount);
+  	// $(ruleShort.find('.voiceAmount')[0]).text(voiceAmount);
+  	// $(ruleShort.find('.videoAmount')[0]).text(videoAmount);
+  	// $(ruleShort.find('.newsAmount')[0]).text(newsAmount);
+  	// var keywordsWrapper = $(ruleShort.find('.keywordsWrapper .val')[0]);
+  	// keywordsWrapper.html('');
+  	// for (var i = 0; i < keywords.length; i++) {
+  	//   keywordsWrapper.append('<font>' + keywords[i] + '</font>');
+  	// }
+  	// rule.hide();
+  	// ruleShort.show();
 }
 
 var bindDeleteRuleAction = function() {
