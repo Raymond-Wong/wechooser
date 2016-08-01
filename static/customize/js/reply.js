bddbc2e42581ee3d573219d758a30a47f6df244e
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-08-01 11:35:59 +0800

    auto submit

--- static/customize/js/reply.js
+++ static/customize/js/reply.js
@@ -68,12 +68,12 @@ var bindReplyAction = function() {
         params['MediaId'] == undefined && params['MediaId'] == 'undefined') {
       topAlert('未选择素材', 'error');
     }
-  	// $.post(url, getMaterialContent(), function(res) {
-  	//   if (res['code'] == 0)
-  	//     topAlert(res['msg']);
-  	//   else
-  	//   	topAlert(res['msg'], 'error');
-  	// });
+  	$.post(url, getMaterialContent(), function(res) {
+  	  if (res['code'] == 0)
+  	    topAlert(res['msg']);
+  	  else
+  	  	topAlert(res['msg'], 'error');
+  	});
   });
   deleteBtn.click(function() {
     var url = window.location.pathname + '/delete' + window.location.search
