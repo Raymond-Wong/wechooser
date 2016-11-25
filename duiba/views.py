843429d4464cf53cd1f8a69b8ff55c96f4621e32
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-11-26 00:15:42 +0800

    auto submit

--- duiba/views.py
+++ duiba/views.py
@@ -18,14 +18,14 @@ import utils
 def getLoginUrl(request):
   # http://www.duiba.com.cn/autoLogin/autologin?uid=test001&credits=100&appKey=jlg88loSQobWDMmGrPLqtmr&sign=fbce303d7ba7ca7b0fe14d576b494769&timestamp=1418625055000
   params = {}
-  params['uid'] = 'test001'
-  params['credits'] = '100'
-  params['appKey'] = 'jlg88loSQobWDMmGrPLqtmr'
+  # params['uid'] = 'test001'
+  # params['credits'] = '100'
+  params['appKey'] = 'testappkey'
   # params['appKey'] = 'xQQjsycj8jSvNCorMkMkCFSZnqK'
-  params['timestamp'] = '1418625055000'
   # params['timestamp'] = str(long(time.time() * 10))
-  params['redirect'] = request.GET.get('dbredirect', None)
+  # params['redirect'] = request.GET.get('dbredirect', None)
   params = utils.filterParam(params)
-  params['sign'] = utils.getSignStr(params, '4PHcHe2h6myutohuwqywuMHNGYMp')
+  params['sign'] = utils.getSignStr(params, 'testappsecret')
+  # params['sign'] = utils.getSignStr(params, '4PHcHe2h6myutohuwqywuMHNGYMp')
   # res = send_request('www.duiba.com.cn', '/autoLogin/autologin', 'GET', port=80, params=params, toLoad=False)
   return HttpResponse(Response(c=0, s="success", m=params).toJson(), content_type='application/json')
