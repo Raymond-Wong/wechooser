d8d557c4d9a66cee664ac223b5a46fc8ab62281a
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-12-07 00:52:39 +0800

    auto submit

--- transmit/views.py
+++ transmit/views.py
@@ -32,7 +32,7 @@ def getNameCard(request):
   # 将用户头像和背景图片结合
   bg = processer.combine(bg, headimg, (80, 80), (184, 202))
   # 在上面得到的背景图片的某个垂直位置放置一个水平居中的用户昵称
-  font_path = '%s/static/transmit/fonts/Deng.ttf' % sys.path[0]
+  font_path = '%s/static/transmit/fonts/Raleway-Regular.ttf' % sys.path[0]
   bg = processer.center_text(bg, user.nickname, 290, (20, 20), font_size=24, font_color="white", font=font_path)
   # 根据用户id生成一个二维码
   coder = qrcode.QRCode(version=5, border=1)
