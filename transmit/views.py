4a82d9689019e501b99a64d59ff16f52d14d3647
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-12-07 00:54:21 +0800

    auto submit

--- transmit/views.py
+++ transmit/views.py
@@ -27,21 +27,21 @@ def getNameCard(request):
   processer = utils.PyImageProcesser()
   # 将用户头像转换成圆形
   headimg = processer.to_circle(headimg)
-  # 打开背景图片
-  bg_path = '%s/static/transmit/images/bg.jpg' % sys.path[0]
-  bg = Image.open(bg_path).convert('RGBA')
-  # 将用户头像和背景图片结合
-  bg = processer.combine(bg, headimg, (80, 80), (184, 202))
-  # 在上面得到的背景图片的某个垂直位置放置一个水平居中的用户昵称
-  font_path = '%s/static/transmit/fonts/Raleway-Regular.ttf' % sys.path[0]
-  bg = processer.center_text(bg, user.nickname, 290, (20, 20), font_size=24, font_color="white", font=font_path)
-  # 根据用户id生成一个二维码
-  coder = qrcode.QRCode(version=5, border=1)
-  coder.add_data(utils.get_user_url(user))
-  coder.make(fit=True)
-  qr_img = coder.make_image()
-  # 将二维码和背景图片合并
-  bg = processer.combine(bg, qr_img, resize=(120, 120), pos=(165, 495), alpha=False)
-  img = bg
+  # # 打开背景图片
+  # bg_path = '%s/static/transmit/images/bg.jpg' % sys.path[0]
+  # bg = Image.open(bg_path).convert('RGBA')
+  # # 将用户头像和背景图片结合
+  # bg = processer.combine(bg, headimg, (80, 80), (184, 202))
+  # # 在上面得到的背景图片的某个垂直位置放置一个水平居中的用户昵称
+  # font_path = '%s/static/transmit/fonts/Raleway-Regular.ttf' % sys.path[0]
+  # bg = processer.center_text(bg, user.nickname, 290, (20, 20), font_size=24, font_color="white", font=font_path)
+  # # 根据用户id生成一个二维码
+  # coder = qrcode.QRCode(version=5, border=1)
+  # coder.add_data(utils.get_user_url(user))
+  # coder.make(fit=True)
+  # qr_img = coder.make_image()
+  # # 将二维码和背景图片合并
+  # bg = processer.combine(bg, qr_img, resize=(120, 120), pos=(165, 495), alpha=False)
+  img = headimg
   img = utils.image_to_base64(utils.image_to_string(img))
   return render_to_response('transmit/showImage.html', {'image' : headimg})
