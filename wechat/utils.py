c4e8f3c590eaf9bb2edf0ab3f71c776544f57f27
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-12-07 23:46:39 +0800

    auto submit

--- wechat/utils.py
+++ wechat/utils.py
@@ -225,7 +225,7 @@ def get_user(openid, token):
   try:
     user = User.objects.get(wx_openid=openid)
     if timezone.now() > user.qrcode_expire_time:
-      qrcode = update_user_qrcode(user)
+      qrcode = update_user_qrcode(user, token)
       user.qrcode_url = qrcode[0]
       user.qrcode_ticket = qrcode[1]
       user.qrcode_expire_time = qrcode[2]
@@ -237,14 +237,14 @@ def get_user(openid, token):
     if not state:
       return False, None
     # 创建用户二维码
-    qrcode = update_user_qrcode(user)
+    qrcode = update_user_qrcode(user, token)
     user.qrcode_url = qrcode[0]
     user.qrcode_ticket = qrcode[1]
     user.qrcode_expire_time = qrcode[2]
     user.save()
     return state, user
 
-def update_user_qrcode(user):
+def update_user_qrcode(user, token):
   host = 'api.weixin.qq.com'
   path = '/cgi-bin/qrcode/create?access_token='
   method = 'POST'
