a89c3a5e2fa07e27b7483634781f1e27bc70232d
Raymond-Wong <raymond.1993.wong@gmail.com>
2016-12-11 15:32:56 +0800

    auto submit

--- transmit/views.py
+++ transmit/views.py
@@ -162,7 +162,7 @@ def invited_by(user, dictionary):
   # 检查邀请用户是否达到目标值
   state, namecard = get_template()
   if invite_user.user_set.count() >= namecard.target:
-    wechat.utils.send_template_msg()
+    wechat.utils.send_template_msg(dictionary['FromUserName'])
   return True, invite_user
 
 def get_template():
