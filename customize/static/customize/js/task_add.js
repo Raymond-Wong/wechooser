ff4a3666e26941c0f6e712f51ed55be162e05c6c
Raymond-Wong <raymond.1993.wong@gmail.com>
2017-01-05 22:33:26 +0800

    auto submit

--- customize/static/customize/js/task_add.js
+++ customize/static/customize/js/task_add.js
@@ -105,7 +105,7 @@ var saveTaskAction = function() {
 }
 
 var initTime = function(dom) {
-  var diff = 1;
+  var diff = 15;
   var now = new Date();
   var minute = now.getMinutes();
   if (minute % diff == 0) {
