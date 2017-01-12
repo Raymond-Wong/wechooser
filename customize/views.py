5eeab0ba406050d3f0a1e08933df5e690c5d6b2f
Raymond-Wong <raymond.1993.wong@gmail.com>
2017-01-12 22:35:07 +0800

    auto submit

--- customize/views.py
+++ customize/views.py
@@ -401,7 +401,7 @@ def list_statistic(request):
   activity_list = Activity.objects.order_by('-create_time')
   aid = int(request.GET.get('aid', '0'))
   records = None
-  if aid != '0':
+  if aid != 0:
     records = Subscribe_Record.objects.filter(record_type=1).filter(record_target=aid)
   else:
     records = Subscribe_Record.objects.filter(record_type=0)
